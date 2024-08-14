8176bb762eca8bfd5256f283cbdfd11b18866476
hardl <philipp.pracht@sap.com>
2024-08-14 19:21:15 +0200

    fix build

--- frontend/src/app/catalog/catalog.component.ts
+++ frontend/src/app/catalog/catalog.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Ui5WebcomponentsModule } from '@ui5/webcomponents-ngx';
+import { IconComponent } from "@ui5/webcomponents-ngx/main/icon";
 
 @Component({
   selector: 'app-catalog',
   standalone: true,
-  imports: [Ui5WebcomponentsModule],
+  imports: [Ui5WebcomponentsModule, IconComponent],
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.scss'
 })
