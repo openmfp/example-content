f05c61f6afc1fb84e94ba7ada9ddd1e517ac762f
Anna Milewska <94687949+amilewskaa@users.noreply.github.com>
2024-08-16 13:51:37 +0200

    Search by categories (#5)
    * Apply styles for card description
    
    * Search by categories
    
    ---------
    
    Co-authored-by: Anna Milewska <anna.milewska@sap.com>

--- frontend/src/app/catalog/catalog-card/catalog-card.component.ts
+++ frontend/src/app/catalog/catalog-card/catalog-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { Ui5WebcomponentsModule } from '@ui5/webcomponents-ngx';
 import { ExtensionClass } from '../../services/extension.schema';
 import { CommonModule } from '@angular/common';
@@ -8,8 +8,13 @@ import { CommonModule } from '@angular/common';
   templateUrl: './catalog-card.component.html',
   styleUrls: ['./catalog-card.component.scss'],
   standalone: true,
-  imports: [Ui5WebcomponentsModule, CommonModule]
+  imports: [Ui5WebcomponentsModule, CommonModule],
 })
 export class CatalogCardComponent {
   @Input() item?: ExtensionClass;
+
+
+  ngOnInit() {
+    // console.log(this.item)
+  }
 }
