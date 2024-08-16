018f33df478cae5c2ae20496b1e34b5a1595a685
Anna Milewska <anna.milewska@sap.com>
2024-08-16 17:23:43 +0200

    Add search by provider

--- frontend/src/app/catalog/catalog.component.ts
+++ frontend/src/app/catalog/catalog.component.ts
@@ -16,9 +16,10 @@ import { CatalogDataService } from '../services/catalog-data.service';
 export class CatalogComponent implements OnInit {
   items?: ExtensionClass[];
   categories: Set<string> = new Set();
-  providers: Set<string> = new Set(); 
+  providers: Set<string> = new Set();
   selectedCategories: string[] = [];
-  filteredItems: ExtensionClass[] = [];
+  selectedProviders: string[] = [];
+  filteredItems!: ExtensionClass[];
 
   constructor(private dataService: CatalogDataService) { }
 
@@ -28,13 +29,43 @@ export class CatalogComponent implements OnInit {
       item.category && this.categories.add(item.category);
       item.provider && this.providers.add(item.provider);
     });
+    this.filteredItems = this.items;
   }
 
   public onCategoriesChange(e: any) {
-    let selectedCategories: string[] = [];
-    e.detail.items.forEach((item: any) => selectedCategories.push(item._state.text));
-    if (this.items) {
-     this.filteredItems = this.items.filter(item => item.category !== undefined && selectedCategories.includes(item.category));
+    this.selectedCategories = [
+      ...e.detail.items.map((item: any) => item._state.text)
+    ];
+    this.filterItems();
+
+  }
+
+  public onProvidersChange(e: any) {
+    this.selectedProviders = [
+      ...e.detail.items.map((item: any) => item._state.text)
+    ];
+    this.filterItems();
+  }
+
+  public filterItems() {
+    if (!this.items) return;
+
+    if (this.selectedCategories.length === 0 && this.selectedProviders.length === 0) {
+      this.filteredItems = this.items;
+      return;
+    } 
+    
+    if (this.selectedCategories.length > 0 && this.selectedProviders.length > 0) {
+      this.filteredItems = this.items.filter(item => 
+        item.category !== undefined && this.selectedCategories.includes(item.category) &&
+        item.provider !== undefined && this.selectedProviders.includes(item.provider)
+      );
+      return;
     }
+
+   this.filteredItems = this.items.filter(item => 
+        item.category !== undefined && this.selectedCategories.includes(item.category) ||
+        item.provider !== undefined && this.selectedProviders.includes(item.provider)
+      );
   }
 }
