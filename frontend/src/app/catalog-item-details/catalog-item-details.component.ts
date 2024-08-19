eeb54936df3766d5baa1e8ec0afb1a8cc6b90169
Waldemar Mazurek <waldemar.mazurek@accenture.com>
2024-08-19 16:48:46 +0200

    Adds option to enable/disable product

--- frontend/src/app/catalog-item-details/catalog-item-details.component.ts
+++ frontend/src/app/catalog-item-details/catalog-item-details.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, inject, OnInit, signal, WritableSignal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { filter } from 'rxjs/operators';
 import { IconComponent, Ui5WebcomponentsModule } from '@ui5/webcomponents-ngx';
 import { CatalogDataService } from '../services/catalog-data.service';
-import { ActivatedRoute } from '@angular/router';
 import { ExtensionClass } from '../services/extension.schema';
 
 @Component({
@@ -10,24 +12,57 @@ import { ExtensionClass } from '../services/extension.schema';
   standalone: true,
   imports: [Ui5WebcomponentsModule, IconComponent, CommonModule],
   templateUrl: './catalog-item-details.component.html',
-  styleUrl: './catalog-item-details.component.scss'
+  styleUrl: './catalog-item-details.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CatalogItemDetailsComponent implements OnInit {
-  catalogItemId?: string;
-  catalogItem?: ExtensionClass;
+  catalogItemId?: string | null;
+  catalogItem: WritableSignal<ExtensionClass | null> = signal(null);
+  buttonVisible: WritableSignal<boolean> = signal(false);
+  private destroyRef = inject(DestroyRef);
 
-  constructor(private route: ActivatedRoute, private dataService: CatalogDataService) { }
+  constructor(private route: ActivatedRoute, private dataService: CatalogDataService) {}
   
-  async ngOnInit() {
-    this.catalogItemId = this.route.snapshot.paramMap.get('catalogItemId') || undefined;
-    if(this.catalogItemId) {
-      this.catalogItem = (await this.dataService.getCatalogItems()).filter(item => {
-        return item.name === this.catalogItemId;
-      })[0];
+  ngOnInit() {
+    this.catalogItemId = this.route.snapshot.paramMap.get('catalogItemId');
+
+    if (!this.catalogItemId) {
+      return;
+    }
+
+    this.setButtonVisibility();
+    this.fetchItemDetails();
+  }
+
+  toggleCatalogItem() {
+    let globalStorage: string[] = JSON.parse(localStorage.getItem('enabled-catalog-items') || '[]');
+
+    if (this.buttonVisible()) {
+      globalStorage.push(this.catalogItemId as string);
+      this.buttonVisible.set(false);
+    } else {
+      globalStorage = globalStorage.filter(name => name !== this.catalogItemId);
+      this.buttonVisible.set(true);
+    }
+
+    localStorage.setItem('enabled-catalog-items', JSON.stringify(globalStorage));
+    
+  }
+
+  private setButtonVisibility() {
+    const globalStorage: string[] = JSON.parse(localStorage.getItem('enabled-catalog-items') || '[]');
+
+    if (!globalStorage.find((name: string) => name === this.catalogItemId)) {
+      this.buttonVisible.set(true);
     }
   }
 
-  enableCatalogItem() {
-    // TODO: logic to enable item here
+  private fetchItemDetails() {
+    this.dataService.fetchCatalogItems();
+    this.dataService.getCatalogItems()
+      .pipe(filter(data => data.length > 0), takeUntilDestroyed(this.destroyRef))
+      .subscribe(data => {
+        this.catalogItem.set(data?.filter(item => item.name === this.catalogItemId)[0] || null);
+      });
   }
 }
--- frontend/src/app/services/catalog-data.service.ts
+++ frontend/src/app/services/catalog-data.service.ts
@@ -1,29 +1,35 @@
 import { Injectable, Injector } from '@angular/core';
-
 import { ENV, Environment } from '../models/env.token';
-import { ExtensionClass, Label } from './extension.schema';
-
+import { ExtensionClass } from './extension.schema';
+import { Observable, Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CatalogDataService {
+  private readonly catalogItems: Subject<ExtensionClass[]> = new Subject<ExtensionClass[]>();
   private readonly env: Environment;
 
   constructor(private injector: Injector) {
     this.env = injector.get(ENV, {});
   }
 
-  async getCatalogItems(account?: string): Promise<[ExtensionClass]> {
-    const data = await (await fetch('./assets/catalog_gql_dump.json')).json();
+  fetchCatalogItems() {
+    fetch('./assets/catalog_gql_dump.json')
+      .then(response => response.json())
+      .then(data => this.catalogItems.next(data?.data?.getExtensionClassesForScopes));
+  }
 
-    // console.log(data);
-    if(account) {
-      const enabled: [string] = JSON.parse(localStorage.getItem('enabled-catalog-items') || '[]');
-      return ((data.data.getExtensionClassesForScopes as [ExtensionClass]).filter(item => {
-        return enabled.indexOf(item.name) >= 0;
-      }) as [ExtensionClass]);
-    }
-    return data.data.getExtensionClassesForScopes;
+  getCatalogItems(account?: string): Observable<ExtensionClass[]> {
+    return this.catalogItems.asObservable().pipe(map(data => {
+      if (account) {
+        const enabled: string[] = JSON.parse(localStorage.getItem('enabled-catalog-items') || '[]');
+    
+        return data.filter(item => enabled.indexOf(item.name) >= 0);
+      }
+
+      return data;
+    }));
   }
 }
