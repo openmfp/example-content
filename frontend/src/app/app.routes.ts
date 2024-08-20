0bfe5f08afb5fdc8e03e32ad06980ec8ed921372
Anna Milewska <94687949+amilewskaa@users.noreply.github.com>
2024-08-20 16:02:41 +0200

    Create dummy pages for Iron Core and Openmcp (#14)
    * Create dummy pages for Iron Core and Openmcp
    
    * Added children routes
    
    ---------
    
    Co-authored-by: Anna Milewska <anna.milewska@sap.com>
    Co-authored-by: Waldemar Mazurek <waldemar.mazurek@accenture.com>

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,26 @@
 import { Routes } from '@angular/router';
 import { CatalogComponent } from './catalog/catalog.component';
 import { CatalogItemDetailsComponent } from './catalog-item-details/catalog-item-details.component';
+import { OpenmcpComponent } from './openmcp/openmcp.component';
+import { IronCoreComponent } from './iron-core/iron-core.component';
 
 export const routes: Routes = [
-    { path: 'global-catalog', component: CatalogComponent },
-    { path: 'catalog-item/:catalogItemId', component: CatalogItemDetailsComponent }
+  { path: 'global-catalog', component: CatalogComponent },
+  {
+    path: 'catalog-item/:catalogItemId',
+    component: CatalogItemDetailsComponent,
+  },
+  {
+    path: 'iron-core',
+    component: IronCoreComponent,
+    children: [
+      { path: 'compute', component: IronCoreComponent },
+      { path: 'network', component: IronCoreComponent },
+    ],
+  },
+  {
+    path: 'openmcp',
+    component: OpenmcpComponent,
+    children: [{ path: 'control-planes', component: OpenmcpComponent }],
+  },
 ];
---
+++ frontend/src/app/iron-core/iron-core.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-iron-core',
+  standalone: true,
+  imports: [],
+  templateUrl: './iron-core.component.html',
+  styleUrl: './iron-core.component.scss'
+})
+export class IronCoreComponent {
+
+}
