78440d84422b8831bfb31c5a71f2e94f4a4dc580
Anna Milewska <anna.milewska@sap.com>
2024-08-20 15:31:29 +0200

    Create dummy pages for Iron Core and Openmcp

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { CatalogComponent } from './catalog/catalog.component';
 import { CatalogItemDetailsComponent } from './catalog-item-details/catalog-item-details.component';
+import { OpenmcpComponent } from './openmcp/openmcp.component';
+import { IronCoreComponent } from './iron-core/iron-core.component';
 
 export const routes: Routes = [
     { path: 'global-catalog', component: CatalogComponent },
-    { path: 'catalog-item/:catalogItemId', component: CatalogItemDetailsComponent }
+    { path: 'catalog-item/:catalogItemId', component: CatalogItemDetailsComponent },
+    { path: 'iron-core', component: IronCoreComponent},
+    { path: 'openmcp', component: OpenmcpComponent}
 ];
---
+++ frontend/src/app/openmcp/openmcp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-openmcp',
+  standalone: true,
+  imports: [],
+  templateUrl: './openmcp.component.html',
+  styleUrl: './openmcp.component.scss'
+})
+export class OpenmcpComponent {
+
+}
