0fdf48c4738a7e3fb2b53c9e6ef73754a33094da
aaronschweig <aaron.schweig@gmail.com>
2024-08-21 12:31:43 +0200

    feat: first implementation of applying contentconfigurations for extensions on account

--- frontend/projects/ui/src/app/catalog-item-details/catalog-item-details.component.ts
+++ frontend/projects/ui/src/app/catalog-item-details/catalog-item-details.component.ts
@@ -1,15 +1,24 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, DestroyRef, inject, OnInit, signal, WritableSignal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  DestroyRef,
+  inject,
+  OnInit,
+  signal,
+  WritableSignal,
+} from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
 import LuigiClient from '@luigi-project/client';
-import { LuigiContextService } from '@luigi-project/client-support-angular';
 import { IconComponent, Ui5WebcomponentsModule } from '@ui5/webcomponents-ngx';
 import { filter } from 'rxjs/operators';
 import { LOCAL_STORAGE_CATALOG_KEY } from '../app.constants';
 import { CatalogContext } from '../models/catalog-model';
 import { CatalogDataService } from '../services/catalog-data.service';
 import { ExtensionClass } from '../services/extension.schema';
+import { PortalLuigiContextService } from '../services/luigi-context.service';
+import { GatewayService } from '../services/gateway.service';
 
 @Component({
   selector: 'app-catalog-item-details',
@@ -17,7 +26,7 @@ import { ExtensionClass } from '../services/extension.schema';
   imports: [CommonModule, IconComponent, Ui5WebcomponentsModule],
   templateUrl: './catalog-item-details.component.html',
   styleUrl: './catalog-item-details.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CatalogItemDetailsComponent implements OnInit {
   account?: string;
@@ -25,9 +34,16 @@ export class CatalogItemDetailsComponent implements OnInit {
   catalogItem: WritableSignal<ExtensionClass | null> = signal(null);
   buttonVisible: WritableSignal<boolean> = signal(false);
   context?: CatalogContext;
+  namespace?: string;
+
   private destroyRef = inject(DestroyRef);
 
-  constructor(private route: ActivatedRoute, private dataService: CatalogDataService, private luigiContextService: LuigiContextService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private dataService: CatalogDataService,
+    private luigiContextService: PortalLuigiContextService,
+    private gatewayService: GatewayService
+  ) {}
 
   ngOnInit() {
     this.catalogItemId = this.route.snapshot.paramMap.get('catalogItemId');
@@ -36,34 +52,75 @@ export class CatalogItemDetailsComponent implements OnInit {
       return;
     }
 
-    this.luigiContextService.getContextAsync().then(ctx => {
+    this.luigiContextService.getContextAsync().then((ctx) => {
       this.account = (ctx['accountId'] || '').trim();
-      this.context = this.account?.length ? CatalogContext.account : CatalogContext.global;
+      this.context = this.account?.length
+        ? CatalogContext.account
+        : CatalogContext.global;
 
       this.setButtonVisibility();
       this.fetchItemDetails();
+      // if (this.account) {
+      //   this.gatewayService
+      //     .getAccountDetails(this.account)
+      //     .pipe(
+      //       tap((res) => {
+      //         this.namespace = res?.status?.namespace ?? '';
+      //       })
+      //     )
+      //     .subscribe();
+      // }
     });
   }
 
   toggleCatalogItem() {
-    const storageKey = this.account ? `${LOCAL_STORAGE_CATALOG_KEY}-${this.account}` : LOCAL_STORAGE_CATALOG_KEY;
-    let globalStorage: string[] = JSON.parse(localStorage.getItem(storageKey) || '[]');
+    const storageKey = this.account
+      ? `${LOCAL_STORAGE_CATALOG_KEY}-${this.account}`
+      : LOCAL_STORAGE_CATALOG_KEY;
+    let globalStorage: string[] = JSON.parse(
+      localStorage.getItem(storageKey) || '[]'
+    );
 
     if (this.buttonVisible()) {
       globalStorage.push(this.catalogItemId as string);
       this.buttonVisible.set(false);
+      if (this.context === CatalogContext.account) {
+        this.gatewayService
+          .enableContentConfiguration(
+            `${this.catalogItemId}-${this.account}`,
+            'openmfp-system'
+          )
+          .subscribe();
+      }
     } else {
-      globalStorage = globalStorage.filter(name => name !== this.catalogItemId);
+      globalStorage = globalStorage.filter(
+        (name) => name !== this.catalogItemId
+      );
+      if (this.context === CatalogContext.account) {
+        this.gatewayService
+          .disableContentConfiguration(
+            `${this.catalogItemId}-${this.account}`,
+            'openmfp-system'
+          )
+          .subscribe();
+      }
       this.buttonVisible.set(true);
     }
 
     localStorage.setItem(storageKey, JSON.stringify(globalStorage));
-    LuigiClient.storageManager().setItem(storageKey, JSON.stringify(globalStorage));
+    LuigiClient.storageManager().setItem(
+      storageKey,
+      JSON.stringify(globalStorage)
+    );
   }
 
   private setButtonVisibility() {
-    const storageKey = this.account ? `${LOCAL_STORAGE_CATALOG_KEY}-${this.account}` : LOCAL_STORAGE_CATALOG_KEY;
-    const globalStorage: string[] = JSON.parse(localStorage.getItem(storageKey) || '[]');
+    const storageKey = this.account
+      ? `${LOCAL_STORAGE_CATALOG_KEY}-${this.account}`
+      : LOCAL_STORAGE_CATALOG_KEY;
+    const globalStorage: string[] = JSON.parse(
+      localStorage.getItem(storageKey) || '[]'
+    );
 
     if (!globalStorage.find((name: string) => name === this.catalogItemId)) {
       this.buttonVisible.set(true);
@@ -72,10 +129,16 @@ export class CatalogItemDetailsComponent implements OnInit {
 
   private fetchItemDetails() {
     this.dataService.fetchCatalogItems();
-    this.dataService.getCatalogItems()
-      .pipe(filter(data => data.length > 0), takeUntilDestroyed(this.destroyRef))
-      .subscribe(data => {
-        this.catalogItem.set(data?.filter(item => item.name === this.catalogItemId)[0] || null);
+    this.dataService
+      .getCatalogItems()
+      .pipe(
+        filter((data) => data.length > 0),
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe((data) => {
+        this.catalogItem.set(
+          data?.filter((item) => item.name === this.catalogItemId)[0] || null
+        );
       });
   }
 }
