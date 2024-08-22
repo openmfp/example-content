f0c6d5f6a4c0df01ce425c81a036c640aa8722a4
hardl <philipp.pracht@sap.com>
2024-08-22 13:46:03 +0200

    enabled catalog mfe

--- frontend/projects/wc/src/app/app.module.ts
+++ frontend/projects/wc/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { ApplicationRef, DoBootstrap, Injector, NgModule } from '@angular/core';
-import { createCustomElement } from '@angular/elements';
+import { createCustomElement, NgElementConstructor } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
 import { DataChartComponent } from './charts/data-chart/data-chart.component';
+import { CatalogComponent } from './catalog/catalog.component';
 
 @NgModule({
   imports: [BrowserModule, DataChartComponent],
@@ -9,11 +10,21 @@ import { DataChartComponent } from './charts/data-chart/data-chart.component';
 export class AppModule implements DoBootstrap {
   constructor(private injector: Injector) {}
 
+
+  registerWebcomponent(component: NgElementConstructor<unknown>, hash: string) {
+    const url = new URL((document.currentScript?.getAttribute('src') as string));
+    if (url.hash === '#' + hash) {
+      (window as any).Luigi._registerWebcomponent(url, component);
+    }
+  }
+
   ngDoBootstrap(appRef: ApplicationRef) {
     const dataChartComponent = createCustomElement(DataChartComponent, {injector: this.injector});
+    const catalogComponent = createCustomElement(CatalogComponent, {injector: this.injector});
 
     try {
-      (window as any).Luigi._registerWebcomponent(document.currentScript?.getAttribute('src'), dataChartComponent);
+      this.registerWebcomponent(dataChartComponent, 'chart');
+      this.registerWebcomponent(catalogComponent, 'catalog');
     } catch (e) {
       if (!customElements.get('data-chart')) {
         customElements.define('data-chart', dataChartComponent);
---
+++ frontend/projects/wc/src/app/catalog/catalog.component.ts
@@ -0,0 +1,36 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnChanges, SimpleChanges, ViewEncapsulation } from '@angular/core';
+import { LuigiClient } from '@luigi-project/client/luigi-element';
+import { Ui5WebcomponentsModule } from '@ui5/webcomponents-ngx';
+
+@Component({
+  selector: 'app-catalog',
+  standalone: true,
+  imports: [CommonModule, Ui5WebcomponentsModule],
+  templateUrl: './catalog.component.html',
+  styleUrls: ['./catalog.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom
+})
+export class CatalogComponent implements OnChanges {
+  @Input() LuigiClient?: LuigiClient;
+  @Input() context?: any;
+  
+  title?: string;
+  dummyData?: string[];
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['context']) {
+      this.title = changes['context'].currentValue.title;
+      this.fetchEnabledExtensions();
+    }
+  }
+
+  fetchEnabledExtensions() {
+    let storageKey = 'Luigi#content.d1.openmfp.dxp.k8s.ondemand.com#enabled-catalog-items';
+    if (this.context.accountId) { // acocuntScope
+      storageKey += '-' + this.context.accountId;
+    } 
+    
+    this.dummyData = JSON.parse(localStorage.getItem(storageKey) || '[]');
+  }
+}
