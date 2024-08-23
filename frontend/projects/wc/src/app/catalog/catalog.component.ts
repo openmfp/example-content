549cd988618eb59da95fef4b9613109233c79c77
Waldemar Mazurek <waldemar.mazurek@accenture.com>
2024-08-23 17:16:17 +0200

    Adds doughnut charts

--- frontend/projects/wc/src/app/catalog/catalog.component.ts
+++ frontend/projects/wc/src/app/catalog/catalog.component.ts
@@ -3,11 +3,12 @@ import { Component, Input, OnChanges, SimpleChanges, ViewEncapsulation } from '@
 import { LuigiClient } from '@luigi-project/client/luigi-element';
 import { Ui5WebcomponentsModule } from '@ui5/webcomponents-ngx';
 import { DataChartComponent } from '../charts/data-chart/data-chart.component';
+import { DoughnutChartComponent } from '../charts/doughnut-chart/doughnut-chart.component';
 
 @Component({
   selector: 'app-catalog',
   standalone: true,
-  imports: [CommonModule, DataChartComponent, Ui5WebcomponentsModule],
+  imports: [CommonModule, DataChartComponent, DoughnutChartComponent, Ui5WebcomponentsModule],
   templateUrl: './catalog.component.html',
   styleUrls: ['./catalog.component.scss'],
   encapsulation: ViewEncapsulation.ShadowDom
@@ -17,17 +18,27 @@ export class CatalogComponent implements OnChanges {
   @Input() context?: any;
   dummyData?: string[];
   title = 'Resource Overview';
+  readonly instanceData = {
+    color: '#AD49E1',
+    total: 100,
+    value: 60
+  };
+  readonly clusterData = {
+    color: '#41B3A2',
+    total: 100,
+    value: 80
+  };
   readonly chartData = {
     labels: ['CW14', 'CW15', 'CW16', 'CW17', 'CW18', 'CW19', 'CW20'],
     datasets: [
       {
-        label: 'Revenue',
+        label: 'Idle',
         data: [431000, 491000, 485000, 536000, 670000, 680000, 659000],
         backgroundColor: '#0070f2',
         borderColor: '#0070f2'
       },
       {
-        label: 'Costs',
+        label: 'Active',
         data: [230000, 238000, 221000, 280000, 230000, 250000, 325000],
         backgroundColor: '#c87b00',
         borderColor: '#c87b00'
