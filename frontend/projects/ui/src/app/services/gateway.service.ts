d8721762ef47f00dde4854a057d9c5ddd3e50c3b
aaronschweig <aaron.schweig@gmail.com>
2024-08-23 14:35:33 +0200

    feat: more submenu entries

--- frontend/projects/ui/src/app/services/gateway.service.ts
+++ frontend/projects/ui/src/app/services/gateway.service.ts
@@ -34,133 +34,159 @@ const deleteContentConfiguration = gql`
 export class GatewayService {
   constructor(private readonly apollo: Apollo) {}
 
+  private generateChildren(name: string) {
+    const length = Math.floor(Math.random() * (3 - 1 + 1) + 1);
+    return new Array(length).fill(0).map((_, idx) => ({
+      pathSegment: 'child' + idx,
+      label: `Child ${name} ${idx}`,
+      category: name,
+      entityType: 'main.account',
+      loadingIndicator: {
+        enabled: false,
+      },
+      viewUrl:
+        'https://fiddle.luigi-project.io/examples/microfrontends/multipurpose.html',
+      context: {
+        title: `Welcome to the ${name} child extension`,
+        content: ' ',
+      },
+    }));
+  }
+
   private buildContentConfiguration(name: string) {
-    return `{
-  "name": "${name}",
-  "creationTimestamp": "2022-05-17T11:37:17Z",
-  "luigiConfigFragment": [
-    {
-      "data": {
-        "nodes": [
-          {
-            "pathSegment": "${name}",
-            "label": "${name}",
-            "entityType": "main.account",
-            "loadingIndicator": {
-              "enabled": false
-            },
-            "url": "https://fiddle.luigi-project.io/examples/microfrontends/multipurpose.html",
-            "context": {
-              "title": "Welcome to the ${name} extension",
-              "content": " "
-            }
-          },{
-            "entityType": "main.account.dashboard::compound",
-            "url": "https://luigiwebcomponents.gitlab.io/ms-adaptive-cards/main.js",
-            "context": {
-              "title": "Activated Services from Marketplace",
-              "style": "padding: 1rem",
-              "disableBorder": true,
-              "card": {
-                "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
-                "type": "AdaptiveCard",
-                "version": "1.0",
-                "speak": "<s>The forecast for Seattle January 20 is mostly clear with a High of 51 degrees and Low of 40 degrees</s>",
-                "body": [
-                  {
-                    "type": "TextBlock",
-                    "text": "${name}",
-                    "size": "large",
-                    "isSubtle": true,
-                    "wrap": true
-                  },
-                  {
-                    "type": "TextBlock",
-                    "text": "Mon, Nov 4, 2019 6:21 PM",
-                    "spacing": "none",
-                    "wrap": true
-                  },
-                  {
-                    "type": "ColumnSet",
-                    "columns": [
+    return JSON.stringify({
+      name: name,
+      creationTimestamp: '2022-05-17T11:37:17Z',
+      luigiConfigFragment: [
+        {
+          data: {
+            nodes: [
+              {
+                pathSegment: name,
+                label: name,
+                entityType: 'main.account',
+                loadingIndicator: {
+                  enabled: false,
+                },
+                category: {
+                  label: name,
+                  icon: 'dimension',
+                  collapsible: true,
+                },
+                url: 'https://fiddle.luigi-project.io/examples/microfrontends/multipurpose.html',
+                context: {
+                  title: `Welcome to the ${name} extension`,
+                  content: ' ',
+                },
+              },
+              ...this.generateChildren(name),
+              {
+                entityType: 'main.account.dashboard::compound',
+                url: 'https://luigiwebcomponents.gitlab.io/ms-adaptive-cards/main.js',
+                context: {
+                  title: 'Activated Services from Marketplace',
+                  style: 'padding: 1rem',
+                  disableBorder: true,
+                  card: {
+                    $schema:
+                      'http://adaptivecards.io/schemas/adaptive-card.json',
+                    type: 'AdaptiveCard',
+                    version: '1.0',
+                    speak:
+                      '<s>The forecast for Seattle January 20 is mostly clear with a High of 51 degrees and Low of 40 degrees</s>',
+                    body: [
                       {
-                        "type": "Column",
-                        "width": "auto",
-                        "items": [
-                          {
-                            "type": "Image",
-                            "url": "https://messagecardplayground.azurewebsites.net/assets/Mostly%20Cloudy-Square.png",
-                            "size": "small",
-                            "altText": "Mostly cloudy weather"
-                          }
-                        ]
+                        type: 'TextBlock',
+                        text: '${name}',
+                        size: 'large',
+                        isSubtle: true,
+                        wrap: true,
                       },
                       {
-                        "type": "Column",
-                        "width": "auto",
-                        "items": [
-                          {
-                            "type": "TextBlock",
-                            "text": "46",
-                            "size": "extraLarge",
-                            "spacing": "none",
-                            "wrap": true
-                          }
-                        ]
+                        type: 'TextBlock',
+                        text: 'Mon, Nov 4, 2019 6:21 PM',
+                        spacing: 'none',
+                        wrap: true,
                       },
                       {
-                        "type": "Column",
-                        "width": "stretch",
-                        "items": [
+                        type: 'ColumnSet',
+                        columns: [
                           {
-                            "type": "TextBlock",
-                            "text": "Hi 50",
-                            "horizontalAlignment": "left",
-                            "wrap": true
+                            type: 'Column',
+                            width: 'auto',
+                            items: [
+                              {
+                                type: 'Image',
+                                url: 'https://messagecardplayground.azurewebsites.net/assets/Mostly%20Cloudy-Square.png',
+                                size: 'small',
+                                altText: 'Mostly cloudy weather',
+                              },
+                            ],
                           },
                           {
-                            "type": "TextBlock",
-                            "text": "Lo 41",
-                            "horizontalAlignment": "left",
-                            "spacing": "none",
-                            "wrap": true
-                          }
-                        ]
-                      }
-                    ]
-                  }
-                ]
-              }
-            },
-            "layoutConfig": {
-              "row": "1",
-              "column": "1 / -1"
-            }
-          }
-        ]
-      }
-    }
-  ]
-}`;
+                            type: 'Column',
+                            width: 'auto',
+                            items: [
+                              {
+                                type: 'TextBlock',
+                                text: '46',
+                                size: 'extraLarge',
+                                spacing: 'none',
+                                wrap: true,
+                              },
+                            ],
+                          },
+                          {
+                            type: 'Column',
+                            width: 'stretch',
+                            items: [
+                              {
+                                type: 'TextBlock',
+                                text: 'Hi 50',
+                                horizontalAlignment: 'left',
+                                wrap: true,
+                              },
+                              {
+                                type: 'TextBlock',
+                                text: 'Lo 41',
+                                horizontalAlignment: 'left',
+                                spacing: 'none',
+                                wrap: true,
+                              },
+                            ],
+                          },
+                        ],
+                      },
+                    ],
+                  },
+                },
+              },
+            ],
+          },
+        },
+      ],
+    });
   }
 
   public getAccountDetails(name: string) {
-    return this.apollo.query<any>({
-      query: gql`
-        query ($name: String!) {
-          coreOpenmfpIo {
-            account(name: $name, namespace: "demo-root") {
-              status {
-                namespace
+    return this.apollo
+      .query<any>({
+        query: gql`
+          query ($name: String!) {
+            coreOpenmfpIo {
+              account(name: $name, namespace: "demo-root") {
+                status {
+                  namespace
+                }
               }
             }
           }
-        }
-      `,
-      variables: {
-        name,
-      },
-    }).pipe(map((res) => res.data?.coreOpenmfpIo?.account ?? {}));
+        `,
+        variables: {
+          name,
+        },
+      })
+      .pipe(map((res) => res.data?.coreOpenmfpIo?.account ?? {}));
   }
 
   public enableContentConfiguration(
