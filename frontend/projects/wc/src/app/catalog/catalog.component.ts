5fa29f22579675d00713e634cf0663870d0b4373
Waldemar Mazurek <waldemar.mazurek@accenture.com>
2024-08-23 14:07:12 +0200

    WC catalog changes (#23)

--- frontend/projects/wc/src/app/catalog/catalog.component.ts
+++ frontend/projects/wc/src/app/catalog/catalog.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component, Input, OnChanges, SimpleChanges, ViewEncapsulation } from '@angular/core';
 import { LuigiClient } from '@luigi-project/client/luigi-element';
 import { Ui5WebcomponentsModule } from '@ui5/webcomponents-ngx';
+import { DataChartComponent } from '../charts/data-chart/data-chart.component';
 
 @Component({
   selector: 'app-catalog',
   standalone: true,
-  imports: [CommonModule, Ui5WebcomponentsModule],
+  imports: [CommonModule, DataChartComponent, Ui5WebcomponentsModule],
   templateUrl: './catalog.component.html',
   styleUrls: ['./catalog.component.scss'],
   encapsulation: ViewEncapsulation.ShadowDom
@@ -17,6 +18,23 @@ export class CatalogComponent implements OnChanges {
 
   title?: string;
   dummyData?: string[];
+  readonly chartData = {
+    labels: ['CW14', 'CW15', 'CW16', 'CW17', 'CW18', 'CW19', 'CW20'],
+    datasets: [
+      {
+        label: 'Revenue',
+        data: [431000, 491000, 485000, 536000, 670000, 680000, 659000],
+        backgroundColor: '#0070f2',
+        borderColor: '#0070f2'
+      },
+      {
+        label: 'Costs',
+        data: [230000, 238000, 221000, 280000, 230000, 250000, 325000],
+        backgroundColor: '#c87b00',
+        borderColor: '#c87b00'
+      }
+    ]
+  };
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes['context']) {
