af82bd1479413a7de44e34590f7ebe6f902a182f
Rafał Rogoda <rafal.rogoda@sap.com>
2025-02-14 17:09:55 +0100

    feat/develop-example-content-for-openmfp-update

--- frontend/projects/wc/src/app/showcase/showcase.component.ts
+++ frontend/projects/wc/src/app/showcase/showcase.component.ts
@@ -30,8 +30,16 @@ export class ShowcaseComponent {
   showcaseItems: ShowcasePanel[] = [
     {
       header: 'Definition of an Entity',
-      label: `Entity is basic building block of configuration, here is example how definition of entity should look like. Configuration consist nodes and texts.`,
-      linkToExample: "firstExample",
+      label: `
+        An entity is the basic building block of configuration. Here is an example of how the definition of an entity should look.
+        The configuration consists of nodes and texts. In this example, {{firstExampleTitle}} will be replaced with the value of
+        "firstExampleTitle" according to the language(locale). The overall structure consists of two nodes. The top node, starting with
+        "entityType": "main", is a definition for "entity-definition" page, it is a sub-page for an entity that is not available here but contains
+        "defineEntity" key with "id": "main". "compound" informs us about the page content. The content itself is defined in another entity
+        starting with "entityType": "main.first-example::compound". The entity type refers to the parent node. "url" points to the component,
+        and "context" lets you pass additional information to the component.
+      `,
+      linkToExample: "entity-definition",
       example: `
         {
           "name": "entity-definition-example",
@@ -41,10 +49,10 @@ export class ShowcaseComponent {
               "nodes": [
                 {
                   "entityType": "main",
-                  "pathSegment": "firstExample",
+                  "pathSegment": "entity-definition",
                   "hideFromNav": true,
                   "defineEntity": {
-                    "id": "first-example"
+                    "id": "entity-definition"
                   },
                   "compound": {
                     "renderer": {
@@ -56,7 +64,7 @@ export class ShowcaseComponent {
                   }
                 },
                 {
-                  "entityType": "main.first-example::compound",
+                  "entityType": "main.entity-definition::compound",
                   "url": "https://luigiwebcomponents.gitlab.io/layouts/panelHeader.js",
                   "context": {
                     "border": "shadow",
@@ -88,8 +96,15 @@ export class ShowcaseComponent {
     },
     {
       header: 'Iframe based Micro Frontend integration',
-      label: 'Example showcasing entity configuration letting add Micro Frontend via iframe. Integration entity requires "requiredIFramePermissions".',
-      linkToExample: "secondExample",
+      label: `
+        This example showcases the configuration of an entity that allows adding a Micro Frontend via an iframe. The integration entity
+        requires "requiredIFramePermissions", consider removing unnecessary permissions. Another important element in this example
+        is the "virtualTree" parameter, which activates navigation around the microservice. "navigationContext" allows you to set
+        the navigation base url. In this way, you will notice that the browser URL gets updated according to the microservice's internal
+        routing as well as the window content. Loading external services may take a while, so you can use a "loadingIndicator" to let
+        the user know about the processing.
+      `,
+      linkToExample: "micro-frontend-iframe",
       example: `
         {
           "name": "micro-frontend-iframe-example",
@@ -99,12 +114,27 @@ export class ShowcaseComponent {
               "nodes": [
                 {
                   "entityType": "main",
-                  "pathSegment": "secondExample",
+                  "pathSegment": "micro-frontend-iframe",
                   "virtualTree": true,
                   "navigationContext": "showcase",
-                  "url": "https://example-content.some-domain.com/ui/example-content/ui/index.html#/showcase",
+                  "urlSuffix": "/ui/example-content/ui/index.html#/showcase",
                   "loadingIndicator": {
                     "enabled": false
+                  },
+                  "requiredIFramePermissions": {
+                    "sandbox": [
+                      "allow-forms",
+                      "allow-modals",
+                      "allow-popups",
+                      "allow-popups-to-escape-sandbox",
+                      "allow-same-origin",
+                      "allow-scripts",
+                      "allow-download"
+                    ],
+                    "allow": [
+                      "clipboard-read",
+                      "clipboard-write"
+                    ]
                   }
                 }
               ]
@@ -115,7 +145,12 @@ export class ShowcaseComponent {
     },
     {
       header: 'Left side menu navigating',
-      label: `Configuration for two entities lets you find two tabs on the left side menu "First Tab" and "Second Tab".`,
+      label: `
+        The configuration for two entities allows you to find two tabs in the left-side menu: "First Tab" and "Second Tab". Both definitions
+        were placed as part of the children array, meaning both are sub-sites of the main entity. This dependency defines the path
+        as follows: "/home/first". To hide a tab in the navigation panel, you can use "hideFromNav": true, or remove the "label".
+        This effect may be useful if you want to keep the navigation functional without making it accessible from the navigation panel.
+      `,
       example: `
         {
           "name": "left-side-navigation-example",
@@ -148,6 +183,14 @@ export class ShowcaseComponent {
                       },
                       "compound": {
                       }
+                    },
+                    {
+                      "pathSegment": "third",
+                      "defineEntity": {
+                        "id": "third"
+                      },
+                      "compound": {
+                      }
                     }
                   ]
                 },
@@ -168,6 +211,15 @@ export class ShowcaseComponent {
                     "title": "Second page",
                     "description": "This is second page"
                   }
+                },
+                {
+                  "entityType": "main.third::compound",
+                  "url": "https://luigiwebcomponents.gitlab.io/layouts/panelHeader.js",
+                  "context": {
+                    "border": "shadow",
+                    "title": "Third page",
+                    "description": "This is third page"
+                  }
                 }
               ]
             }
@@ -177,8 +229,13 @@ export class ShowcaseComponent {
     },
     {
       header: 'Integrate Web Component into page',
-      label: 'Entity can have definition for Web Component, "url" points to the component, "content" pass configuration data over to component. In this example component is registered under name "account-overview"',
-      linkToExample: "fourthExample",
+      label: `
+        An entity can have a definition for a Web Component. The "urlSuffix" points to the component, and "content" passes configuration data
+        over to the component. In this example, the component is registered under the name "account-overview". To use the component,
+        you should add the "webcomponent" key with "selfRegistered": true; otherwise, the interpreter won't recognize your definition
+        as a webcomponent setup.
+      `,
+      linkToExample: "web-component-integration",
       example: `
         {
           "name": "web-component-integration-example",
@@ -188,17 +245,17 @@ export class ShowcaseComponent {
               "nodes": [
                 {
                   "entityType": "main",
-                  "pathSegment": "fourthExample",
+                  "pathSegment": "web-component-integration",
                   "hideFromNav": true,
                   "defineEntity": {
-                    "id": "fourth-example"
+                    "id": "web-component-integration"
                   },
                   "compound": {
                   }
                 },
                 {
-                  "entityType": "main.fourth-example::compound",
-                  "url": "https://example-content.some-domain.com/ui/wc/main.js#account-overview",
+                  "entityType": "main.web-component-integration::compound",
+                  "urlSuffix": "/ui/example-content/wc/main.js#account-overview",
                   "context": {
                     "title": "Showcase"
                   },
